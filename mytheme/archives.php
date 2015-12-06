ffe25d84a20d194b49c5690fcc9a91d72b491613
otemomi <to.8581.mn@gmail.com>
2015-12-06 10:12:27 +0900

    Revert "固定ページのarchivesのページ送り実装"
    This reverts commit 59e7f26970233d031fadca9142c669fa88c981b3.

--- mytheme/archives.php
+++ mytheme/archives.php
@@ -7,42 +7,19 @@ get_header(); ?>
 <main>
 	<div id="contents">
 		<div id="main">
-			<h2 class="archiveH"><?php the_title(); ?></h2>
-			<?php
-			$paged = (int) get_query_var('paged');
-			$args = array(
-				'posts_per_page' => 10,
-				'paged' => $paged,
-				'orderby' => 'post_date',
-				'order' => 'DESC',
-				'post_type' => 'post',
-				'post_status' => 'publish'
-			); ?>
-			<?php $the_query = new WP_Query($args);
-			if ( $the_query->have_posts() ) :
-				while ( $the_query->have_posts() ) : $the_query->the_post();
-					get_template_part( 'content', get_post_format() ); ?>
-
-				<ul>
-					<li class="mb10"><?php the_date(); ?> : <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-				</ul>
-
-			<?php endwhile;
-			else:
-				get_template_part( 'content', 'none' );
-			endif; ?>
-			<div class="pager">
-			<?php if ($the_query->max_num_pages > 1) {
-				echo paginate_links(array(
-					'base' => get_pagenum_link(1) . '%_%',
-					'format' => 'page/%#%/',
-					'current' => max(1, $paged),
-					'total' => $the_query->max_num_pages
-				));
-			}; ?>
-			</div>
-			<?php wp_reset_postdata(); ?>
-
+				<h2 class="archiveH"><?php the_title(); ?></h2>
+					<?php
+						$args = array( 'posts_per_page' => 20, 'order'=> 'DESC', 'orderby' => 'date' );
+						$postslist = get_posts( $args );
+						foreach ( $postslist as $post ) :
+						  setup_postdata( $post ); ?>
+						<ul>
+							<li class="mb10"><?php the_date(); ?> : <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+						</ul>
+					<?php
+						endforeach;
+						wp_reset_postdata();
+					?>
 		</div>
 	</div>
 
--- mytheme/js/script.js
+++ mytheme/js/script.js
@@ -12,7 +12,7 @@ $(function() {
 	topBtn.click(function () {
 		$('body,html').animate({
 			scrollTop: 0
-		}, 250);
+		}, 500);
 		return false;
 	});
 });
